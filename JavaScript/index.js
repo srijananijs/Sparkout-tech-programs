d98544e1427382cfe1b6ba2e123bb95e176216f2
Sri Janani <srijananishankarganesh@gmail.com>
2022-02-28 17:55:35 +0530

    commit

---
+++ JavaScript/index.js
@@ -0,0 +1,14 @@
+alert("heyy");
+console.log("Sri");
+var a = 20;
+console.log(a);
+prompt("what is your name");
+a += 10;
+console.log(a);
+var i = 1;
+let sum = 0;
+for(i=0;i<100;i++)
+{
+    sum = sum + 1;
+    console.log(sum);
+}
